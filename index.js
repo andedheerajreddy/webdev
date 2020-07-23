2ec10fbd4f372ac8d9bfd858dfdb1a5856ab37e4
andedheerajreddy <adreddy.jmd@gmail.com>
2020-07-23 16:13:55 +0530

    added html

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express= require('express');
 var app=express();
 
@@ -8,14 +7,3 @@ res.sendFile(__dirname+"/basic.html");
 
 var port=process.env.PORT||3000;
 app.listen(port,()=>{console.log("server started....")})
-=======
-var express= require('express');
-var app=express();
-
-app.get("/",function(req,res){
-res.send("Hello world !");
-});
-
-var port=process.env.PORT||3000;
-app.listen(port,()=>{console.log("server started....")})
->>>>>>> cfd225f532c10ab7f4bfa2ecfb11f602986c477d
