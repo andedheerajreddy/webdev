ebf4ae33baec883e0cdc56ec164fce1a0df24d12
andedheerajreddy <adreddy.jmd@gmail.com>
2020-07-25 13:42:10 +0530

    resume

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app=express();
 app.use(express.static('frontend'));
 
 app.get('/',(req,res)=>{
+    res.sendFile(__dirname+"/frontend/html/home.html");  
+})
+app.get('/resume',(req,res)=>{
     res.sendFile(__dirname+"/frontend/html/index.html");
 })
 app.listen(process.env.PORT||4000,()=>{console.log("server started running");})
